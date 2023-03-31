2e462cb530e22ee2c78f694025ad430d0385bd0e
techtube770 <128456787+techtube770@users.noreply.github.com>
2023-03-31 16:48:18 +0530

    Add files via upload

---
+++ Triangularbutton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Triangularbutton = () => {
+    return (
+        <div>
+          <a href="#">
+            <span>Button</span>
+            <span>Button</span>
+          </a>
+        </div>
+      );
+}
+
+export default Triangularbutton
+
